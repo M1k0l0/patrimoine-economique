12986c5894f48c1b56b171544cec5321145fac40
Mikolo <hei.mikolo.2@gmail.com>
2024-08-27 20:04:46 +0300

    feat: add endpoints and make modification in app.jsx file

--- backend/main.js
+++ backend/main.js
@@ -1,10 +1,30 @@
 import express from "express";
 import {readFile} from "../data/index.js";
+import cors from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
 app.get("/possession",async(req,res) =>{
     res.send(await readFile("./data.json"))
 })
+app.get("/patrimoine/:valeur",async(req,res) =>{
+    const valeur = req.params.valeur
+    const data = await readFile("./data.json")
+
+    const possessions = data.data[1].data.possessions
+    const filterPossession = () => {
+       return possessions.filter((valeur) => possessions.valeur <= valeur )
+    }
+    res.send(filterPossession())
+})
+
+app.post("/newValue",async(req,res) => {
+    const body = req.body
+    res.send(body)
+})
+
 app.listen(3000,() => {
     console.log("Je teste")
 })
